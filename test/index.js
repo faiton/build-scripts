37d58298ec538af0363bf5241506ec618afd5e9c
Mushex Antaranian <mushex@antaranian.com>
2014-08-14 19:43:38 +0400

    first

---
+++ test/index.js
@@ -0,0 +1,47 @@
+var resolve = require('component-resolver');
+var join = require('path').join;
+var assert = require('assert');
+var vm = require('vm');
+
+var Build = require('./build.js');
+
+describe('example', function () {
+  var build;
+
+  var fixture = join(__dirname, 'fixtures/example');
+
+  it('should resolve', function (done) {
+    resolve(fixture, {
+      install: true,
+    }, function (err, tree) {
+      if (err) return done(err);
+
+      build = Build(tree);
+      done();
+    })
+  })
+
+  it('should build scripts', function (done) {
+    build.scripts(function (err, js) {
+      console.log(js);
+      if (err) return done(err);
+
+      var ctx = vm.createContext();
+      vm.runInContext(js, ctx);
+      done();
+    })
+  })
+
+  it('should build scripts with UMD', function (done) {
+    build.set('umd', 'asdf');
+
+    build.scripts(function (err, js) {
+      if (err) return done(err);
+
+      var ctx = vm.createContext();
+      vm.runInContext(js, ctx);
+      vm.runInContext('if (!this.asdf) throw new Error();', ctx);
+      done();
+    })
+  })
+})
